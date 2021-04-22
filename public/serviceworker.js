1f2dd385cbbf132717bd4ebf93deb39a27b6b6ce
Troy Dorman <troydorman@gmail.com>
2021-04-22 14:58:11 -0600

    implemented the fetch event listener in the service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -17,6 +17,13 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(() => {
+            return fetch(event.request)
+                .catch(() => caches.match('offline.html'))
+        })
+    )
 
 });
 
